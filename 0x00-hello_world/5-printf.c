90cf35fcd3d045445dc838eb32032a7816461f97
Soukaina4621 <soukaina.mer99@gmail.com>
2023-07-13 18:26:56 -0700

    all files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program printing with printf function.
+ *
+ * Return: ALWAYS 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
